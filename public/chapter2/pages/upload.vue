10d7cf6e63f390b9308bac0d95b26032abf47a5f
18621032336 <1351978299@qq.com>
2018-04-25 15:42:51 +0800

    Create upload.vue

---
+++ public/chapter2/pages/upload.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <file-button @output="output" maxSize="100">
+            点击上传
+        </file-button>
+        <img v-if="src" :src="src">
+    </div>
+</template>
+<script>
+    import FileButton from '../components/FileButton'
+
+    export default {
+        components: {
+            FileButton
+        },
+        data() {
+            return {
+                src: '',
+            };
+        },
+        created() {
+            console.log('created11111')
+        },
+        methods: {
+            output(data) {
+                console.log(data)
+                this.src = data;
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
