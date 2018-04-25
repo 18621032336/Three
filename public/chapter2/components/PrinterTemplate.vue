4f25ca164d1ad3ca9bc258d6dbf00e7e81e7d7df
18621032336 <1351978299@qq.com>
2018-04-25 15:43:34 +0800

    Create PrinterTemplate.vue

---
+++ public/chapter2/components/PrinterTemplate.vue
@@ -0,0 +1,32 @@
+<template>
+    <div ref="container">
+        <h1 :key="i" v-for="(item,i) in data.list">{{item}}</h1>
+
+        <h2 v-if="data.if">条件显示内容</h2>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                data: {}
+            }
+        },
+        updated() {
+            this.callback(this.$refs.container.innerHTML);
+        },
+        methods: {
+            myRender(data) {
+                return new Promise((resolve, reject) => {
+                    this.callback = function (template) {
+                        console.log('template', '---', template)
+                        resolve(template);
+                    };
+                    this.data = { // 设置数据
+                        ...data,
+                    };
+                })
+            }
+        }
+    }
+</script>
