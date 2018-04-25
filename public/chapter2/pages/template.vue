dc348a2e2de9d78b9cc9d07f52d366008ec5b0e6
18621032336 <1351978299@qq.com>
2018-04-25 15:42:32 +0800

    Create template.vue

---
+++ public/chapter2/pages/template.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <printer-template ref="printer"></printer-template>
+        <button @click="handler">点击渲染模板</button>
+    </div>
+</template>
+<script>
+    import PrinterTemplate from '../components/PrinterTemplate'
+
+    export default {
+        components: {
+            PrinterTemplate
+        },
+        data() {
+            return {};
+        },
+        created() {
+            console.log('created11111')
+        },
+        methods: {
+            async handler(e) {
+                let data = {
+                    list: ['1', '2', '3', '4', '5', '6'],
+                    if: false,
+                }
+                let template = await this.$refs.printer.myRender(data);
+                console.log(template)
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
